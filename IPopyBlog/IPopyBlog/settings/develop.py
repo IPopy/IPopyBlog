d99d2a3b7546dea419cdee57618d26c13e608dac
zcy <spzhangcy@gmail.com>
2019-06-04 15:51:45 +0800

    Feat:建立三个 APP，连接 MySQL 数据库

--- IPopyBlog/IPopyBlog/settings/develop.py
+++ IPopyBlog/IPopyBlog/settings/develop.py
@@ -7,7 +7,13 @@ DEBUG = True
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ipopyblog',
+        'USER': 'root',
+        'PASSWORD': '5854258',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
     }
 }
